7802c5f4f21d5e72ba1ad24e6cca00943e0fa2cf
Nayana <Nayana@appinsight.tech>
2022-11-18 09:07:46 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/alrudo_pos/store/alrudo_pos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alrudo_pos.store.alrudo_pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feautures/grocbay_pos/presentation/bloc/homepage/subcategory/subCategory_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import '../../../../../../core/error/failuers.dart';
+import '../../../../domain/entities/fetch_subcategory.dart';
+import '../../../../domain/usecases/get_sub_category.dart';
+
+part 'subCategory_event.dart';
+
+part 'subCategory_state.dart';
+
+class SubcategoryBloc extends Bloc<SubcategoryEvent, SubcategoryState> {
+  SubcategoryBloc(GetSubCategory getSubCategory) : super(SubcategoryStateInitial()) {
+    on<SubcategoryEvent>((event, emit) async{
+      // TODO: implement event handler
+      emit(SubcategoryStateLoading());
+      final value = await getSubCategory(data: SubCatParms(allkey:"All", catId: event.props.first as int));
+      value.fold((l) => emit(SubcategoryStateError(mapFailureToMessage(l))), (r) => emit(SubcategoryStateSucsess<List<FetchSubCategory>>(r)));
+      // TODO: implement event handler
+
+    });
+  }
+}
